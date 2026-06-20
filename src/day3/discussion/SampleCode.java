f96c9f8c2b058e40cf0059318a6bf30eeae550b6
Maynard <johnmaynardgabriel.dev@gmail.com>
2026-06-20 21:20:10 +0800

    Add discussion code for day2 and day3

---
+++ src/day3/discussion/SampleCode.java
@@ -0,0 +1,17 @@
+
+package day3.discussion; //package
+
+public class SampleCode { //class
+    
+    public static void main(String[] args){ //main method
+        System.out.println("Hello, World!"); //print statement
+    }
+}
+
+//main method - > starting point of programs execution
+
+/*
+    2 types of comments
+    1. Single Line Comment
+    2. Multi Line Comment
+*/
