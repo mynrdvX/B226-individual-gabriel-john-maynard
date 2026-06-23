c460f70a90380025edb023a6618cff0814f8a0d4
Maynard <johnmaynardgabriel.dev@gmail.com>
2026-06-23 16:05:40 +0800

    Added discussion for Day 5

---
+++ src/day5/discussion/ArithmeticOperator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day5.discussion;
+
+public class ArithmeticOperator {
+    public static void main(String[] args) {
+        // + - * / %
+        int num = 55;
+        int num2 = 10;
+        
+        System.out.println("Total: " + (num + num2));
+        System.out.println(num - num2);
+        System.out.println(num * num2);
+        System.out.println(num / num2);
+        System.out.println(num % num2);
+    }
+}
---
+++ src/day5/discussion/UnaryOperator.java
@@ -0,0 +1,21 @@
+
+package day5.discussion;
+
+
+public class UnaryOperator {
+    public static void main(String[] args) {
+        //negating and expression (-)
+        int num = -10;
+        System.out.println(num);
+        
+        //inverting the value of a boolean (!)
+        boolean isEnrolled = false;
+        System.out.println(!isEnrolled);
+        
+        //increment / decrement a value by one (++ / --)
+        int number = 50;
+        
+        System.out.println(number++);
+        System.out.println(number--);
+    }
+}
