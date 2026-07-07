dd0acddaab175099753992bc2b80fc636898f74d
Maynard <johnmaynardgabriel.dev@gmail.com>
2026-07-07 16:52:33 +0800

    Add code for Day 17 discussion

---
+++ src/day17/discussion/Ballpen.java
@@ -0,0 +1,26 @@
+
+package day17.discussion;
+
+//will act as a bluprint of an object that you want to implement
+public class Ballpen {
+    
+    //attributes / fields / placeholder
+    String brand;
+    String color;
+    float pointSize;
+    double weightInGrams;
+    
+    
+    //purpose / method
+    public void write(){
+        System.out.println("Writing: ");
+    }
+    
+    public void draw(){
+        System.out.println("Drawing");
+    }
+    
+    public void sketch(){
+        System.out.println("Sketching");
+    }
+}
