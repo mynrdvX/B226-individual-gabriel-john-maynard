5b8f03b2ce53b46b23beb02af15331438bd3e86c
Maynard <johnmaynardgabriel.dev@gmail.com>
2026-07-16 20:19:39 +0800

    Add Day 24 Code for discussion of Abstraction

---
+++ src/day24/discussion/interfacee/Main.java
@@ -0,0 +1,14 @@
+
+package day24.discussion.interfacee;
+
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Person person = new Father();
+        person.role();
+        
+        
+        
+    }
+}
